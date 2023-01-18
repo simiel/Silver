8edd48a753a6117e994d1b4b5c6be24e6664fffd
Simiel <samensah036@st.ug.edu.gh>
2023-01-18 13:10:06 +0000

    added project views with templates pending

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+def project_detail(request, p_key):
+    project = Project.objects.get(pk=p_key)
+    context = {'project'=project}
+    return render(request,"project_detail.html", context)
