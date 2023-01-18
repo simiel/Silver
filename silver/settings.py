77020c944ed3ffc9241bc77b7e8d16c0ed6dc4dc
Simiel <samensah036@st.ug.edu.gh>
2023-01-18 12:36:18 +0000

    Hello Wiase App

--- silver/settings.py
+++ silver/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'silver.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        "DIRS": ["silver/templates/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
