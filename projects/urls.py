da47713b816f0042c0361294402ea36717420a9a
Simiel <samensah036@st.ug.edu.gh>
2023-01-18 13:47:50 +0000

    Added templates and run server

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", )
+    path("", views.project_index, name="project_index"),
+    path('<int:pk>/', views.project_detail, name='project_detail')
 ]
--- projects/views.py
+++ projects/views.py
@@ -8,7 +8,7 @@ def project_index(request):
     }
     return render(request, 'project_index.html', context)
 
-def project_detail(request, p_key):
-    project = Project.objects.get(pk=p_key)
-    context = {'project'=project}
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {'project' : project}
     return render(request,"project_detail.html", context)
